ccb94b3afef6b450a8b3a236dd9d41c96f95e961
farmer169 <132373279+farmer169@users.noreply.github.com>
2023-05-15 23:14:44 +0800

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -64,20 +64,7 @@ if __name__ == '__main__':
                  'duration_prefer16', 'interact_prefer1', 'interact_prefer2', 'interact_prefer3', 'interact_prefer4',
                  'interact_prefer5', 'interact_prefer6', 'interact_prefer7', 'interact_prefer8', 'interact_prefer9',
                  'interact_prefer10', 'interact_prefer11']]
-    # from sklearn.preprocessing import StandardScaler
-    # label = data['label']
-    # data = data.drop(columns=['label'])
-    # ss = StandardScaler()
-    # for col in data.columns:
-    #     data[col] = ss.fit_transform(data[col].values.reshape(-1, 1))
-    # data['label'] = label
-    # print(data)
-
-    # data["launch_seq"] = data.launch_seq.apply(lambda x: json.loads(x))
-    # data["playtime_seq"] = data.playtime_seq.apply(lambda x: json.loads(x))
-    # data["duration_prefer"] = data.duration_prefer.apply(lambda x: json.loads(x))
-    # data["interact_prefer"] = data.interact_prefer.apply(lambda x: json.loads(x))
-
+    
     kf = KFold(n_splits=10, random_state=12, shuffle=False)
     result_dict = {'F1': [], 'AUC': [], 'ACC': [], 'Recall': [], 'Precision': []}
     i = 0
@@ -85,7 +72,6 @@ if __name__ == '__main__':
         i += 1
         print("-------------------{}------------------".format(i))
         train, test_data = data.loc[train_index], data.loc[test_index]
-        # train, test_data = data[train_index, :], data[test_index, :]
         train, dev = train_test_split(train, test_size=0.05, random_state=42)
         print(train.shape, type(train))
         test_data = test_data.reset_index(drop=True)
@@ -95,20 +81,11 @@ if __name__ == '__main__':
         train = DataGenerator(train, 256)
 
         model = build_model(feature_num=38)
-
-        # scheduler = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-        #     boundaries=[2, 5, 8],
-        #     values=[0.01, 0.008, 0.005, 0.003]
-        # )
-        # model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=scheduler), loss="binary_crossentropy",
-        #               metrics=['accuracy', keras.metrics.AUC()])
-
         model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss="binary_crossentropy",
                       metrics=['accuracy', keras.metrics.AUC()])
         early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=3, restore_best_weights=True)
         model.fit(iter(train), steps_per_epoch=len(train), validation_data=iter(dev), validation_steps=len(dev),
                   epochs=20, callbacks=[early_stopping])
-        # callbacks=[early_stopping]
         model.save("Data/churn_mlp_best_weights.h5")
         prediction = model.predict(iter(test), steps=len(test))
         predictions = [1 if prediction[i][0] > 0.5 else 0 for i in range(prediction.shape[0])]
@@ -137,29 +114,6 @@ if __name__ == '__main__':
     df_result.to_csv('Data/dnn_results.csv', index=False)
     print(df_result.mean().T)
 
-    # prediction = model.predict(iter(dev), steps=len(dev))
-    # print(prediction)
-    # predictions = [1 if prediction[i][0] > 0.5 else 0 for i in range(prediction.shape[0])]
-    # print(predictions)
-    # print(predictions.count(1))
-    # print(predictions.count(0))
-    # y_test = data.loc[:30000]['label']
-    #
-    # print(y_test)
-    # auc = roc_auc_score(y_test, prediction)
-    # precision = precision_score(y_test, predictions)
-    # recall = recall_score(y_test, predictions)
-    # f1 = f1_score(y_test, predictions)
-    # print('F1: %f  Precision:%f  Recall:%f  AUC:%f' % (f1, precision, recall, auc))
-    # F1: 0.673999  Precision:0.636058  Recall:0.716754  AUC:0.652044
-
-
-    #              0.0005              0.001                 0.01               0.005             0.1
-    # F1           0.656822           0.687562              0.375739            0.598199         0.433741
-    # AUC          0.681290           0.697995              0.543554            0.603801         0.500468
-    # ACC          0.644161           0.658517              0.544814            0.596872         0.504694
-    # Recall       0.695767           0.746024              0.428221            0.626944         0.573681
-    # Precision    0.631356           0.637759              0.424844            0.594903         0.355614
 
 
 
